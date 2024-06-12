1b1a9f9b2df695ae64c7cdd446fb20263cae4d75
Fathima Fida K <fidafirozkattungal@gmail.com>
2024-06-12 23:25:41 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import "./App.css"
 import travelPic1 from "./assets/rome1.jpeg"
 import travelPic2 from "./assets/london1.jpeg"
 import travelPic3 from "./assets/europe1.jpeg"
+import travelPic4 from "./assets/switzerland.jpg"
+import travelPic5 from "./assets/malaysia.jpg"
+import travelPic6 from "./assets/iceland.jpeg"
 function App() {
   return (
     <>
@@ -19,7 +22,11 @@ function App() {
       
     <TravelCard place="Rome,Italy" duration="10 Days Trip" price="$5,42k" image={travelPic1} />
     <TravelCard place="London,uk" duration="12 Days Trip"   price="$4.2k" image={travelPic2}/>
-    <TravelCard place="Full Europe" duration="28 Days Trip"   price="$15k"  image={travelPic3}/>
+    <TravelCard place="Europe " duration="28 Days Trip"   price="$15k"  image={travelPic3}/>
+          
+    <TravelCard place="Switzerland" duration="1 Month Trip" price="$223.00k" image={travelPic4} />
+    <TravelCard place="Malaysia" duration="15 Days Trip"   price="$34.55k" image={travelPic5}/>
+    <TravelCard place="Iceland" duration="5 Days Trip"   price="$234.78k"  image={travelPic6}/>
     {/* <Text/>
       <Header/>
       <Food/>
